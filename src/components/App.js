c88ce2f34ea0ebe74794e2009d4d5bcb8fc783f9
Code-250 <richmunye@gmail.com>
2021-05-20 07:16:58 +0200

    form-input-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
-import "./App.css";
-import HomePage from "../pages/homepage/Homepage";
-import shopPage from "../pages/shop/shopPage.component";
-import Header from "../header/header.component";
-import { Switch, Route } from "react-router-dom";
+import './App.css';
+import HomePage from '../pages/homepage/Homepage';
+import shopPage from '../pages/shop/shopPage.component';
+import Header from '../header/header.component';
+import { Switch, Route } from 'react-router-dom';
+import SignIn from '../pages/signInAndSignUp/signIn';
 function App() {
   return (
     <>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={shopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={shopPage} />
+        <Route path='/signIn' component={SignIn} />
       </Switch>
     </>
   );
